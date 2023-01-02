8e199beb632bb6273a06d97e36655ab06f2b0e85
MakotoIsNotAvailable <kuzunoha@gmail.com>
2023-01-02 13:04:02 +0800

    sama

--- Matematika/main.py
+++ Matematika/main.py
@@ -1,44 +1,55 @@
-import random
 import os
+import random
+import time
+#import marshal as mas
 
+op = '[ © ]'
+kon = False
+print(f'{op}Memeriksa Data...')
+time.sleep(1)
+cek = os.path.exists('Data')
+if cek is True:
+	os.chdir('Data')
+	cekh = os.path.exists('soal.py')
+	cekhj = os.path.exists('data.txt')
+	cekhg = os.path.exists('__init__.py')
+	if cekh is True and cekhj is True and cekhg is True:
+		print(f'{op}Memproses Data...')
+		from Data import *
+		with open('data.txt','r') as file:
+			nilai = int(file.read())
+			time.sleep(2)
+		print(f'{op}All Done\n{op}')
 
-cek = os.path.exists('data.txt')
-if cek is False:
-	with open('data.txt',mode='w') as file:
-		file.write('0')
+	elif cekh is True and cekhg is True and cekhj is False:
+		print(f'{op}Memproses Data...')
+		time.sleep(1)
+		print(f'{op}data.txt Tidak Di Temukan,Membuat data Baru...')
+		with open('data.txt','w') as file:
+			file.write('0')#### Pake Pickle
+			nilai = 0
+			time.sleep(1)
+		from Data import *
+		print(f'{op}All Done\n{op}')
+	elif cekh is False or cekhg is False:
+		print(f'{op}Data Tidak Ditemukan !')
+		print(f'{op}Tidak Bisa Menjalankan Program')
+		kon = True
+else:
+	print(f'{op}Data Tidak Ditemukan\n{op}Tidak Bisa Menjalankan Program')
+	print('Tidak Bisa Menjalankan Program')
+	kon = True
 
-#
+if kon is False:
+	for i in [5,4,3,2,1]:
+		print(f'{op}Program Akan Di Mulai Dalam {i}')
+		time.sleep(1)
+	print(op)
 
-#
-
-
-with open('data.txt','r') as file:
-	nilai = int(file.read())
-
-
-def soal1():
-	global nilai
-	a = random.randint(0,99)
-	b = random.sample(range(0,a),1)
-	q = ''.join(random.sample('+-',1))
-	print(f'[ ~ ] {a} {q} {b[0]} = ')
-	jawab = int(input('Jawab: '))
-	if jawab == 1+1:
-		print('Jawaban Benar')
-		nilai += 1
-	else:
-		print('Jawaban Salah')
-		
 
 
 
 while True:
-	print(f'Nilai = {nilai}')
-	soal1()
-
-
-
-
-#Ketika Program Baru Di Jalankan, Program Akam Mengecek Apakah Ada Data File Yang Bernama data.txt
-#Jika Tidak Ada Maka Program Akan Membuat File Dengan Nama data.txt Dan Menulis Kedalam Text Tersebut Nama Dam Nilai, Nama Di Ambil Dari Input,Sedangkan Nilai Adalah 0
-#Jika Ada Maka Program Akan Memindahkan Nama Dan Nilai Ke Dalam Sebuah Variable
+	with open('data.txt','r') as file:
+		print('Nilai = ',file.read())
+	a1()
