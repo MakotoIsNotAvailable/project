260a26e4aef61e1068c841e95c03781b8c1a6a95
MakotoIsNotAvailable <kuzunoha@gmail.com>
2023-01-07 16:13:37 +0800

    Membuat Program Enkripsi Text

--- enkripsi/Main.py
+++ enkripsi/Main.py
@@ -1,33 +1,48 @@
 
 # #Membuat Dict Yang Isinya Alphabet
-chara = {' ':32}
-def sp():
-	print(end='\n\n\n')
-def new(a,b):
-	global chara
-	for i in range(a,b+1):
-		chara[chr(i)] = i
-new(65,90)#Upper
-new(97,122)#Lower
-print(chara)
+#chara = {' ':32}
+#def sp():
+#	print(end='\n\n\n')
+#def new(a,b):
+#	global chara
+#	for i in range(a,b+1):
+#		chara[chr(i)] = i
+#new(65,90)#Upper
+#new(97,122)#Lower
+#print(chara)
 
-sp()
-
-
+#sp()
 
 
 
 
 
-kunci = {}
-outText = ''
+
+
 #	#Encrypt Messege
-key1 = list('KoRe Wa NaNI')
-text = 'HeheTeNandayoH'
+pesan = 'Hel O wh aTTshu p  Bnah'
+pesanList = []
+#	#Mengubah Pesan Dan Kunci  Menjadin Sekumpulan Angka/Ord Sesuai Indexnya
+for i in pesan:
+	pesanList.append(ord(i))
 
-print(key1)
+key1 = 'IzbIVeisvI'
+key1List = []
+for i in key1:
+	key1List.append(ord(i))
 
-for i in text:
-	chara[i] + 2
+#print(pesanList)
+#print(key1List)
 
-print(outText)
+#	#Enkripsi
+x1 = 0
+for i in range(len(pesanList)+1):
+	#print(i)
+	if x1 >= len(key1List):
+		x1 -= len(key1List)
+	print(pesanList[i])
+	#pesanList[i] ** key1List[x1]
+	x1 += 1
+
+
+
