59bd1a59deaa22809d031a7495ac8c0a2c22807d
MakotoIsNotAvailable <kuzunoha@gmail.com>
2023-01-10 20:08:16 +0800

    Melanjutkan Project Data Management

--- dataManagemen/MTS-SMP.py
+++ dataManagemen/MTS-SMP.py
@@ -1,4 +1,17 @@
 
 
 class Raport:
-	def __init__
+	def __init__(self,nama,nisn):
+		self.nama = nama
+		self.nisn = nisn
+	
+	def kelas7(self):
+		pass
+	
+	def setNilaiP(self,kelas,prakarya):
+		self.kelas.prakarya = prakarya
+
+
+bruh = Raport('aspian','0083936728')
+
+bruh.setNilaiP()
