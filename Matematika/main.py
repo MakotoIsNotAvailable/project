e8dd78dccee0adc249177babc6d999dd8b89bde2
MakotoIsNotAvailable <kuzunoha@gmail.com>
2023-01-31 20:34:06 +0800

    Terjadi Perubahan Hehe

--- Matematika/main.py
+++ Matematika/main.py
@@ -1,44 +1,97 @@
+print('Memuat Program...')
 import os
 import random
 import time
 
-op = "[ © ]"
-program = False
-timer = False
-
-def turu(a):
-	if timer is True: time.sleep(a)
-
-
-
-print(f'{op}Memeriksa Data...')
-cek = os.path.exists('Data')
-if timer is True: time.sleep(1)
-if cek is True: 
-	os.chdir('Data')
-	cekj = os.path.exists('soal.py')
-	cekjh = os.path.exists('__init__.py')
-	cekjhg = os.path.exists('file.txt')
-	cekjhgf = os.path.exists('log.txt')
-	if cekj is True and cekjh is True:
-		program = True is False:
-			print(f"{op}Tidak Ada Save File\n{op}Membuat File Baru")
-			with open('file.txt',mode=w) as f:
-				f.write('0')
-				nilai = 0
-		
-		elif cekjhg is True:
-			with open('file.txt','w') as f:
-				nilai = int(f.read())
-				
-		if cekjhgf is False: #Log
-			pass
-
-
-
-
-#os.remove('file.txt')
-
-while program:
-	print('Anjay Bisa Jalan.')
+def load():
+	print('Memuat Data User...')
+	cek = os.path.exists('data.txt')
+#	if cek is True:
+#		with open('data.txt','r') as f:
 	
+def ver():
+	while True:
+		try:
+			jawab = int(input())
+
+			ver = int(input('Yakin? [1/2] '))
+			if ver == 1:
+				return jawab
+				break
+			elif ver == 2:
+				print('Masukkan Ulang Jawaban: ',end='')
+				continue
+		except ValueError:
+				print('Input Tidak Valid!\nMasukkan Ulang Jawaban:',end=" ")
+
+def randnum(a,b):
+	return random.randint(a,b)
+	
+def soal():
+	global no
+	global nilai
+	operasi = ''.join(random.sample('+-×÷',1))
+#	operasi = '+'
+
+	print('Operasi =',operasi)
+
+	if operasi == '+':
+#		a = random.randint(0,100)
+#		b = random.randint(0,100)
+		a = randnum(0,50)
+		b = randnum(0,50)
+		print(f'{no}. {a} + {b} = ',end='')
+		if ver() == a + b:
+			print('Yeay')
+			nilai += 1
+		else:
+			print('Jawaban Salah!\nJawaban Yang Benar: {a+b}')
+			nilai -= 1
+
+	elif operasi == "-":
+		a = randnum(0,50)
+		b = randnum(0,a) 
+		print(f"{no}. {a} - {b} = ",end="")
+		if ver() == a - b:
+			print("Yaho")
+			nilai += 1
+		else:
+			print('Jawaban Salah!\nJawaban Yang Benar: {a+b}')		
+			nilai -= 1
+
+#	elif operasi == '×':
+#		print(f'{no}. {a} × {b} = ',end='')
+#		if ver() == a * b:
+#			print('Yeay')
+#			nilai += 1
+#		else:
+#			print('Jawaban Salah!\nJawaban Yang Benar: {a*b}')
+#			nilai -= 1
+#		
+#	if operasi == '÷':
+#		a = randnum(0,50)
+#		b = randnum(0,50)
+#		print(f'{no}. {a} + {b} = ',end='')
+#		if ver() == a + b:
+#			print('Yeay')
+#			nilai += 1
+#		else:
+#			print('Jawaban Salah!\nJawaban Yang Benar: {a/b}')
+#			nilai -= 1
+
+
+	no += 1
+
+nilai = 0
+no = 1
+print('Succes')
+if __name__ == '__main__':
+	print('Program Dimulai Dalam 3 Detik...')
+#	time.sleep(3)
+	while True:
+		print(f'Nilai = {nilai}')
+		soal()
+
+# Fitur:
+#	waktu penyelesaian 
+#	save and load
