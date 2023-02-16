8d5a07ab89dc9e02711a2c00898f00dcf39dcb78
MakotoIsNotAvailable <kuzunoha@gmail.com>
2023-02-17 06:56:36 +0800

    Melakukan update pada project math_v1

---
+++ Z_Trash/textToOrd.py
@@ -0,0 +1,6 @@
+a = 'sudo_suH_adalah_password_dari_main_gmail#2'
+
+
+for i in list(a):
+	print(ord(i),end='')
+
--- math_v1/Main.py
+++ math_v1/Main.py
@@ -16,23 +16,27 @@ def save():
 	pass
 
 # #Bilangan random untuk soal
-def randnum(a,b):
+def randnum(a=0,b=100):
 	return random.randint(a,b)
 
-# #Riwayat user
-def log():
-	print('Menyimpan riwayat pemain...')
-	
-	def save_log(ls:list):
+# Fungsi Tambahan Untuk log
+def save_log(ls:list):
 		with open("log.csv","w",newline="",encoding="utf-8") as csv_file:
 			csv.writer(csv_file,delimiter= ",").writerows(ls)
 			csv_file.close()
-	
+
+# #Riwayat user
+def log():
+	# Cara Kerja:
+		#Menbaca File, kemudian menyimpan nya kedalam sebuah list, lalu ditambahkan data baru, setelah itu di tulis lagi
+		
+	print('Menyimpan riwayat pemain...')	
 	now = time.strftime('%d-%B-%Y,%H:%M:%S')
 	data = [[now,no,nilai]]	
 	
 	xlog = os.path.exists("log.csv")
 	if xlog == False:
+		#Membuat File Csv Jika Tidak Ada
 		print("Log tidak di temukan\nMembuat log...")
 		data.insert(0,["Tanggal","No","Nilai"])
 		save_log(data)
@@ -42,29 +46,35 @@ def log():
 			data_old = [i for i in csv.reader(file,delimiter=",")]
 			data_old.append(data[0])
 			save_log(data_old)
-	print('Succes')
+	print('Tersimpan')
+	os.system('clear')
 	
 
-def ver():
+def jawab(v=True):
+	# Fungsi ini berfungsi supaya tidak terjadi runtime eror, fungsi ini akan mengembalikan nilai yang di input pada variable input_user
 	while True:
 		try:
-			jawab = float(input())	
-			ver = int(input('Yakin? [1/2] '))
-			if ver == 1:
-				return jawab
+			input_user = float(input())	
+			if v == True:
+				ver = int(input('Yakin? [1/2] '))
+				if ver == 1:
+					return input_user
+					break
+				else:
+					print('Masukkan Ulang Jawaban: ',end='')
+			elif v == False:
+				return input_user
 				break
-			elif ver == 2:
-				print('Masukkan Ulang Jawaban: ',end='')
 		except ValueError:
 				print('Input Tidak Valid!\nMasukkan Ulang Jawaban:',end=" ")
 
 
 	
-def soal():	
-	log()
-	
+def soal():
 	global no
 	global nilai
+	log()
+	
 	operasi = ''.join(random.sample('+-×÷',1))
 	operasi = '+'
 
@@ -74,7 +84,7 @@ def soal():
 		a = randnum(0,50)
 		b = randnum(0,50)
 		print(f'{no}. {a} + {b} = ',end='')
-		if ver() == a + b:
+		if jawab() == a + b:
 			print('Yeay')
 			nilai += 1
 		else:
@@ -85,7 +95,7 @@ def soal():
 		a = randnum(0,50)
 		b = randnum(0,a) 
 		print(f"{no}. {a} - {b} = ",end="")
-		if ver() == a - b:
+		if jawab() == a - b:
 			print("Yaho")
 			nilai += 1
 		else:
@@ -93,16 +103,18 @@ def soal():
 			nilai -= 1
 
 	elif operasi == '×':
-		a = randnum(0,50)
-		b = randnum(0,20)
+		a = randnum(0,30)
+		b = randnum(0,10)
 		print(f'{no}. {a} × {b} = ',end='')
-		if ver() == a * b:
+		if jawab() == a * b:
 			print('Yeay')
 			nilai += 1
 		else:
 			print(f'Jawaban Salah!\nJawaban Yang Benar: {a*b}')
 			nilai -= 1
-		
+	
+	# Later
+#######################
 	elif operasi == '÷':
 		dummy = randnum(0,50)
 		print('Dummy =' ,dummy)
@@ -112,28 +124,32 @@ def soal():
 #		b = randnum(0,a)
 		b = 2
 		print(f'{no}. {a} ÷ {b} = ',end='')
-		if ver() == a / b:
+		if jawab() == a / b:
 			print('Jawaban Benar')
 			nilai += 1
 		else:
 			print(f'Jawaban Salah!\nJawaban Yang Benar: {a/b}')
 			nilai -= 1
 	no += 1
-
+##############################
 print('Succes')
-
-
 nilai = 0
 no = 1
 if __name__ == '__main__':
-	print('Program Dimulai Dalam 3 Detik...')
-#	time.sleep(3)
-#	os.system('clear')
+	if os.path.exists('log.csv') == False:
+		log()
+	with open('log.csv','r',newline="",encoding='utf-8') as file:
+		a = [i for i in csv.reader(file,delimiter=",")]
+		nilai = a[-1][2]; nilai = int(nilai)
+		no = a[-1][1]; no = int(no)
+			
+	print(f'Nilai = {nilai}')
+	print(f'No = {no}')
+	print(type(nilai),type(no))
+	
+	input('Klik Enter Untuk Melanjutkan')	
 	while True:
-		print(f'Nilai = {nilai}')
-		print(f'No = {no}')
 		soal()
-		input('Pause')
 
 # Fitur:
 #	waktu penyelesaian 
